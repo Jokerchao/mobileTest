b498f616ce012cfd7bf054a12db4fc037b1ba423
kraos <980737054@qq.com>
2025-01-24 14:49:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mobiletest/MainActViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.mobiletest
+
+/**
+ * @author kraos
+ * @date 2025/1/23
+ */
+class MainActViewModel {
+
+}
---
+++ app/src/main/java/com/example/mobiletest/repo/BookingRepository.kt
@@ -0,0 +1,8 @@
+package com.example.mobiletest.repo
+
+/**
+ * @author kraos
+ * @date 2025/1/23
+ */
+object BookingRepository {
+}
